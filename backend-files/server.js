1a93e1fb86dd728adfded58e2d7483fccc77d724
wikd <tomisiok@gmail.com>
2025-05-12 20:12:11 +0200

    gallery changes + one more change

--- backend-files/server.js
+++ backend-files/server.js
@@ -9,7 +9,6 @@ app.use(express.json());
 app.use(cors({origin: 'http://localhost:4200', credentials: true}));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// Setup MySQL connection pool
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
@@ -26,34 +25,31 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-// Upload endpoint
 app.post('/upload', upload.single('image'), async (req, res) => {
   const { user_id } = req.body;
-  const filePath = req.file.path;
   const fileName = req.file.filename;
 
   await pool.query(
-    'INSERT INTO files (userId, file_name, file_path) VALUES (?, ?, ?)',
-    [user_id, fileName, filePath]
+    'INSERT INTO files (userId, file_name) VALUES (?, ?)',
+    [user_id, fileName]
   );
 
-  res.json({ success: true, fileName, filePath });
+  res.json({ success: true, fileName });
 });
 
 // Gallery endpoint
 app.get('/gallery/:userId', async (req, res) => {
   const userId = req.params.userId;
   const [rows] = await pool.query(
-    'SELECT fileId, file_name, file_path FROM files WHERE userId = ?',
+    'SELECT fileId, file_name FROM files WHERE userId = ?',
     [userId]
   );
 
-  // Return file URLs
   const baseUrl = req.protocol + '://' + req.get('host');
   const gallery = rows.map(img => ({
     id: img.fileId,
     fileName: img.file_name,
-    url: `${baseUrl}/${img.file_path}`,
+    url: `${baseUrl}/uploads/${img.file_name}`,
   }));
 
   res.json(gallery);
@@ -64,7 +60,7 @@ app.delete('/gallery/:userId/:fileId', async (req, res) => {
 
   try {
     await pool.query(
-      'DELETE FROM files WHERE fileId = ? AND userId = ?',
+      'DELETE FROM files WHERE fileId = ? AND userId = ? ',
       [fileId, userId]
     );
 
@@ -79,7 +75,8 @@ app.get('/gallery', async (req, res) => {
   try {
     const [result] = await pool.query(
       `SELECT * , users.login AS username FROM files 
-      JOIN users ON files.userId = users.Uid`
+      JOIN users ON files.userId = users.Uid 
+      ORDER BY uploaded_at DESC`
     );
 
     const baseUrl = req.protocol + '://' + req.get('host');
@@ -89,7 +86,7 @@ app.get('/gallery', async (req, res) => {
       id: img.fileId,
       userId: img.userId,
       fileName: img.file_name,
-      url: `${baseUrl}/${img.file_path}`,
+      url: `${baseUrl}/uploads/${img.file_name}`,
       uploaded_at: img.uploaded_at,
       username: img.username
     }));
@@ -100,6 +97,6 @@ app.get('/gallery', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-// Start server
+
 const PORT = 3001;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
--- src/app/communitygallery/communitygallery.component.ts
+++ src/app/communitygallery/communitygallery.component.ts
@@ -1,19 +1,23 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { GalleryImage, GalleryService } from '../services/gallery.service';
 
 @Component({
   selector: 'app-communitygallery',
-  imports: [NgFor],
+  imports: [NgFor, NgIf, FormsModule],
   templateUrl: './communitygallery.component.html',
   styleUrl: './communitygallery.component.css'
 })
 export class CommunitygalleryComponent {
   gallery: GalleryImage[] = [];
+  searchUsername: string='';
 
   constructor(private galleryService: GalleryService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {this.loadGallery();
+  }
+  loadGallery(){
     this.galleryService.getCommunityGallery().subscribe({
       next: (images) => this.gallery = images,
       error: (err) => console.error('Error loading gallery:', err)
